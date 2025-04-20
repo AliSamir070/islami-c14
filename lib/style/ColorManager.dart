08bfbf52c75bc29862fd9834cd806993aa18582f
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-20 13:07:22 +0200

    adding new colors

--- lib/style/ColorManager.dart
+++ lib/style/ColorManager.dart
@@ -3,4 +3,10 @@ import 'dart:ui';
 abstract final class ColorManager{
   static const Color primary = Color(0xffE2BE7F);
   static const Color blackColor = Color(0xff202020);
+  static const Color white = Color(0xffFFFFFF);
+  static const Color offWhite = Color(0xffFEFFE8);
+  static const Color gray = Color(0xff707070);
+  static const Color brown = Color(0xff946107);
+  static const Color red = Color(0xffFE4039);
+  static const Color orange = Color(0xffFFAD3B);
 }
