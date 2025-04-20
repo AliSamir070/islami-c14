c021344365a505e54e1f3012f760ba264069fab3
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-20 13:07:09 +0200

    adding app images

---
+++ lib/core/constants/app_images.dart
@@ -0,0 +1,7 @@
+abstract final class AppImages{
+  // Images
+  static const String sebhaUpperPart = "assets/images/sebha_upper_part.png";
+  static const String sebhaBody = "assets/images/sebha_body.png";
+  static const String islamiMosqueLogo = "assets/images/islami_mosque_logo.png";
+  static const String sebhaScreenBackground = "assets/images/sebha_screen_background.png";
+}
