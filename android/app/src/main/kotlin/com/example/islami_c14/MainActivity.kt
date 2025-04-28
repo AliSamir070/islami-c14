1eba2b0b0840b07171842aa79114f7594d7cbddd
Ali070 <ali.zidan39@yahoo.com>
2025-04-29 00:47:26 +0300

    + finish tasbeh tab

--- android/app/src/main/kotlin/com/example/islami_c14/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.islami_c14
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/ui/home/tabs/sebha_tab/SebhaTab.dart
+++ lib/ui/home/tabs/sebha_tab/SebhaTab.dart
@@ -1,10 +1,96 @@
 import 'package:flutter/material.dart';
 
-class SebhaTab extends StatelessWidget {
+class SebhaTab extends StatefulWidget {
   const SebhaTab({super.key});
 
   @override
+  State<SebhaTab> createState() => _SebhaTabState();
+}
+
+class _SebhaTabState extends State<SebhaTab> {
+  double _angle=6;
+  int _counter=0;
+  List<String> azkar=["سبحان الله","الحمدلله","الله اكبر"];
+  int currentAzkarIndex=0;
+  @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.grey,);
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: BoxDecoration(
+        image: DecorationImage(image: AssetImage("assets/images/sebha_back.png"),
+        fit: BoxFit.cover)
+      ),
+      child: Column(
+        children: [
+        Image.asset("assets/images/islami.png",
+        width: 299,
+        height: 141,),
+        Text("سَبِّحِ اسْمَ رَبِّكَ الأعلى ",
+          style:TextStyle(
+            fontWeight: FontWeight.w700,
+            fontSize: 36,
+            color: Colors.white
+          )),
+          SizedBox(height: 40,),
+          Expanded(
+              child:Stack(
+                clipBehavior: Clip.none,
+               alignment: Alignment.topCenter,
+                children: [
+                  Image.asset("assets/images/Group 37.png"),
+                  Positioned(
+                    top: 78,
+                      child: Transform.rotate(
+                        angle: _angle,
+                        child: Image.asset("assets/images/SebhaBody 1.png",
+                          height: 300,
+                          width:300 ,),
+                      )),
+                  Positioned(
+                   top: 170,
+                    child: InkWell(
+                      onTap:(){
+                        _angle -=20;
+                        setState(() {});
+                        _tabOnSebha();
+                      },
+                      child: Column(
+                        children: [
+                          Text(azkar[currentAzkarIndex %azkar.length],
+                          style: TextStyle(
+                            fontSize: 36,
+                            fontWeight: FontWeight.w700,
+                            color: Colors.white,
+                          ),
+                          ) ,
+                          Text("$_counter",
+                          style: TextStyle(
+                            fontSize: 36,
+                            fontWeight: FontWeight.w700,
+                            color: Colors.white,
+                          ),
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+
+                ],
+              )
+          )
+        ],
+      ),
+    );
+
+  }
+
+  void _tabOnSebha() {
+    _counter++;
+    if (_counter==33){
+      _counter=0;
+      currentAzkarIndex++;
+    }
+    setState(() {});
   }
 }
